44e79b8ef68df37188ec09582cf7e443644304d8
Yanjo96 <wlan.kabel578@gmx.net>
2019-10-25 14:04:10 +0200

    2019.10.25.1400

---
+++ TreeTopology/CLIOps.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2.7
+'''
+Command-line execution of annonex2embl
+'''
+
+#####################
+# IMPORT OPERATIONS #
+#####################
+
+import sys
+import os
+
+# Add specific directory to sys.path in order to import its modules
+# NOTE: THIS RELATIVE IMPORTING IS AMATEURISH.
+# NOTE: COULD THE FOLLOWING IMPORT BE REPLACED WITH 'import annonex2embl'?
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'annonex2embl'))
+
+# IMPORTANT: TFL must be after "sys.path.append"
+import TreeTopologyMain
+import argparse
+
+###############
+# AUTHOR INFO #
+###############
+
+__author__ = 'Nils Jenke; Yannick Hartmaring'
+__copyright__ = 'Copyright (C) 2019 Project Gruenstaeudl'
+__info__ = 'TreeTopology'
+__version__ = '2019.10.25'
+
+#############
+# DEBUGGING #
+#############
+
+#import ipdb
+#ipdb.set_trace()
+
+############
+# ARGPARSE #
+############
+
+class CLI():
+
+    def __init__(self):
+        self.client()
+
+    def client(self):
+
+        parser = argparse.ArgumentParser(description="  --  ".join([__author__, __copyright__, __info__, __version__]))
+        parser._action_groups.pop()
+        required = parser.add_argument_group('required arguments')
+        optional = parser.add_argument_group('optional arguments')
+
+
+        ### REQUIRED ###
+        required.add_argument('-a',
+                            '--alignment',
+                            help='absolute path to infile; infile in NEXUS, PHYLIB or FASTA format; Example: /path_to_input/test.nex',
+                            required=True)
+
+        required.add_argument('-c',
+                            '--constraint',
+                            help='absolute path to constraint file; infile in NEWICK format; Example: /path_to_input/tree.txt',
+                            required=True)
+
+        required.add_argument('-o',
+                            '--output',
+                            help='Output Tree in Newick format',
+                            required=True)
+
+        ### OPTIONAL ###
+
+        optional.add_argument('--version',
+                            help='print version number and exit',
+                            action='version',
+                            version='%(prog)s ' + __version__)
+
+        args = parser.parse_args()
+
+        TreeTopologyMain.treetopology(args.alignment,
+                                      args.constraint,
+                                      args.output)
+
+########
+# MAIN #
+########
+
+def start_treetopology():
+    CLI()
---
+++ TreeTopology/TreeTopologyMain.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.7
+
+''' Main operations in TreeTopology '''
+
+#####################
+# IMPORT OPERATIONS #
+#####################
+
+
+###############
+# AUTHOR INFO #
+###############
+
+__author__ = ''
+__copyright__ = ''
+__info__ = 'TreeTopology'
+__version__ = '0'
+
+
+#############
+# DEBUGGING #
+#############
+
+#import ipdb
+#ipdb.set_trace()
+
+
+#############
+# FUNCTIONS #
+#############
+
+
+def treetopology(alignment,
+                 contraint,
+                 output):
+    print("Hello World!")
+
+    # 1. input handler
+
+    # 2. find optimal tree with RAxML or FastTree
+
+    # 3. build trees from contraints and search most likely one
+
+    # 4. is best tree calculated from contraints (3) significant worse than
+    #    the optimal one (2); if yes return (2); otherwise return (3)
